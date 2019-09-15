cb9b49d8b8554fe90320d77d58c35254a4c61735
rogerndaba <rogerndaba@gmail.com>
2019-09-15 17:20:27 +0200

    created broker sql table

---
+++ brokers.sql
@@ -0,0 +1,59 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.6deb5
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Sep 15, 2019 at 05:16 PM
+-- Server version: 5.7.27-0ubuntu0.18.04.1
+-- PHP Version: 7.2.19-0ubuntu0.18.04.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `fixme`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `brokers`
+--
+
+CREATE TABLE `brokers` (
+  `br_id` bigint(20) NOT NULL,
+  `br_name` varchar(255) NOT NULL,
+  `br_username` varchar(255) NOT NULL,
+  `br_password` varchar(255) NOT NULL,
+  `br_deleted` tinyint(1) NOT NULL DEFAULT '0',
+  `br_ip` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `brokers`
+--
+ALTER TABLE `brokers`
+  ADD PRIMARY KEY (`br_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `brokers`
+--
+ALTER TABLE `brokers`
+  MODIFY `br_id` bigint(20) NOT NULL AUTO_INCREMENT;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
