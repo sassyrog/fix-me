31967781f16cf96fefd67d080a7b7fb9d496fe68
Rogerndaba <rogerndaba@gmail.com>
2019-09-20 14:52:46 +0200

    beggining router interaction on broker

--- broker/src/main/java/com/fixme/Broker.java
+++ broker/src/main/java/com/fixme/Broker.java
@@ -48,70 +48,67 @@ public class Broker {
 		return this.auth;
 	}
 
-	// // main client method
-	// public void getResponseFromServer(String request) {
-	// try {
-	// // non blocking client socket
-	// SocketChannel sc = SocketChannel.open();
-	// sc.configureBlocking(false);
+	public void getResponseFromServer(String request) {
+		try {
+			// non blocking client socket
+			SocketChannel sc = SocketChannel.open();
+			sc.configureBlocking(false);
 
-	// InetSocketAddress addr = new InetSocketAddress(hostName, port);
-	// sc.connect(addr);
+			InetSocketAddress addr = new InetSocketAddress(hostName, port);
+			sc.connect(addr);
 
-	// while (!sc.finishConnect()) {
-	// System.out.println("conneting to server");
-	// }
+			while (!sc.finishConnect()) {
+				System.out.println("conneting to server");
+			}
 
-	// // send request
-	// bb.flip();
-	// bb.clear();
-	// bb.put(request.getBytes());
-	// bb.flip();
-	// sc.write(bb);
+			// send request
+			bb.flip();
+			bb.clear();
+			bb.put(request.getBytes());
+			bb.flip();
+			sc.write(bb);
 
-	// // process response
-	// Selector selector = Selector.open();
-	// sc.register(selector, SelectionKey.OP_READ);
-	// while (true) {
-	// if (selector.select() > 0) {
-	// if (processServerResponse(selector)) {
-	// return;
-	// }
-	// }
-	// }
-	// } catch (IOException e) {
-	// e.printStackTrace();
-	// }
-	// }
+			// process response
+			Selector selector = Selector.open();
+			sc.register(selector, SelectionKey.OP_READ);
+			while (true) {
+				if (selector.select() > 0) {
+					if (processServerResponse(selector)) {
+						return;
+					}
+				}
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 
-	// // I need to keep this running
+	public boolean processServerResponse(Selector s) {
+		Iterator<SelectionKey> i = s.selectedKeys().iterator();
+		while (i.hasNext()) {
+			try {
+				SelectionKey sk = i.next();
+				if (sk.isReadable()) {
+					SocketChannel schannel = (SocketChannel) sk.channel();
+					bb.flip();
+					bb.clear();
 
-	// public boolean processServerResponse(Selector s) {
-	// Iterator<SelectionKey> i = s.selectedKeys().iterator();
-	// while (i.hasNext()) {
-	// try {
-	// SelectionKey sk = i.next();
-	// if (sk.isReadable()) {
-	// SocketChannel schannel = (SocketChannel) sk.channel();
-	// bb.flip();
-	// bb.clear();
+					int count = schannel.read(bb);
+					if (count > 0) {
+						bb.rewind();
+						String response = Charset.forName("UTF-8").decode(bb).toString();
+						System.out.println("response: " + response);
 
-	// int count = schannel.read(bb);
-	// if (count > 0) {
-	// bb.rewind();
-	// String response = Charset.forName("UTF-8").decode(bb).toString();
-	// System.out.println("response: " + response);
-
-	// schannel.close();
-	// return true;
-	// }
-	// }
-	// i.remove();
-	// } catch (IOException e) {
-	// e.printStackTrace();
-	// }
-	// }
-	// return false;
-	// }
+						schannel.close();
+						return true;
+					}
+				}
+				i.remove();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		return false;
+	}
 
 }
