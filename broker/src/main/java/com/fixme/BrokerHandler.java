375bae5a235afc0c75c280ccd1da6f66f5317f1f
rogerndaba <rogerndaba@gmail.com>
2019-09-21 11:16:11 +0200

    fucked up on routing table

--- broker/src/main/java/com/fixme/BrokerHandler.java
+++ broker/src/main/java/com/fixme/BrokerHandler.java
@@ -2,10 +2,13 @@ package com.fixme;
 
 import java.util.Scanner;
 
+import com.fixme.controlers.MysqlConnect;
+
 /**
  * BrokerHandler
  */
 public class BrokerHandler {
+	private MysqlConnect conn = MysqlConnect.getDbCon();
 
 	static public String brokerBuy() {
 		Scanner scanner = new Scanner(System.in);
