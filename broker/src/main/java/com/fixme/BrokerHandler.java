8361cbb6201a4f5685cb00a6783fa986cb3d1f11
rogerndaba <rogerndaba@gmail.com>
2019-09-24 14:35:22 +0200

    started on broker bu functiomality

--- broker/src/main/java/com/fixme/BrokerHandler.java
+++ broker/src/main/java/com/fixme/BrokerHandler.java
@@ -1,5 +1,7 @@
 package com.fixme;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.Scanner;
 
 import com.fixme.controlers.MysqlConnect;
@@ -8,19 +10,28 @@ import com.fixme.controlers.MysqlConnect;
  * BrokerHandler
  */
 public class BrokerHandler {
-	private MysqlConnect conn = MysqlConnect.getDbCon();
+	static private MysqlConnect conn = MysqlConnect.getDbCon();
+	static Scanner scanner = new Scanner(System.in);
 
 	static public String brokerBuy() {
-		Scanner scanner = new Scanner(System.in);
-
-		scanner.close();
+		try {
+			System.out.println("Here's the list of all available instruments");
+			ResultSet rSet = conn.query("SELECT inst_name, inst_id, inst_amount FROM instruments");
+			if (!rSet.wasNull()) {
+				rSet.next();
+				System.out.println("inst_name : " + rSet.getString("inst_name"));
+			} else {
+				System.out.println("some stuff");
+			}
+		} catch (SQLException se) {
+			se.printStackTrace();
+			// TODO: handle exception
+		}
 		return "blah";
 	}
 
 	static public String brokerSell() {
-		Scanner scanner = new Scanner(System.in);
 
-		scanner.close();
 		return "blah";
 	}
 }
