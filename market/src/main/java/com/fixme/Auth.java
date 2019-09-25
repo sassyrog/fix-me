c436bd65f9c5dc3a8b783a14650a546f5b81122e
Rogerndaba <rogerndaba@gmail.com>
2019-09-25 09:53:02 +0200

    adding Auth to market

---
+++ market/src/main/java/com/fixme/Auth.java
@@ -0,0 +1,126 @@
+package com.fixme;
+
+import java.io.Console;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.regex.Pattern;
+
+import com.fixme.controlers.MysqlConnect;
+
+import at.favre.lib.crypto.bcrypt.BCrypt;;
+
+/**
+ * Auth
+ */
+public class Auth {
+	private Console console = System.console();
+	private String name;
+	private String username;
+	private String password1;
+	private String password2;
+
+	static private MysqlConnect conn = MysqlConnect.getDbCon();
+
+	public Auth() {
+	}
+
+	public boolean signUp() {
+		name = "";
+		username = "";
+		password1 = "";
+		password2 = "";
+
+		int track = 0;
+		while ((name.equals("") || username.equals("") || password1.equals("")) || track == 0) {
+			System.out.println();
+			name = console.readLine("Broker Name : ");
+			username = console.readLine("Username : ");
+			password1 = new String(console.readPassword("Password : "));
+			password2 = new String(console.readPassword("re-type Password : "));
+
+			System.out.println();
+			if (name.equals(""))
+				System.out.println("\u001B[1;31mBroker Name cannot be empty\u001B[0m");
+			else if (!Pattern.matches("^[a-zA-Z]\'?[-a-zA-Z]+$", name))
+				System.out.println("\u001B[1;31mName not right\u001B[0m");
+			else if (username.equals(""))
+				System.out.println("\u001B[1;31mUsername cannot be empty\u001B[0m");
+			else if (!Pattern.matches("^[a-zA-Z0-9_]+$", username))
+				System.out.println("\u001B[1;31mUsername not right\u001B[0m");
+			else if (password1.equals(""))
+				System.out.println("\u001B[1;31mPassword cannot be empty\u001B[0m");
+			else if (!password2.equals(password1))
+				System.out.println("\u001B[1;31mPasswords don't match\u001B[0m");
+			else {
+				String coor = console.readLine("\u001B[1;37mIs all the info above correct? (y|n) (Y|N) : \u001B[0m");
+				if (coor.equals("y") || coor.equals("Y"))
+					track++;
+			}
+		}
+		signUpDB();
+		return true;
+	}
+
+	public boolean login() {
+		username = "";
+		password1 = "";
+		while (username.equals("") || password1.equals("")) {
+			System.out.println();
+			username = console.readLine("Username : ");
+			password1 = new String(console.readPassword("Password : "));
+
+			if (username.equals(""))
+				System.out.println("\u001B[1;31mUsername cannot be empty\u001B[0m");
+			else if (password1.equals(""))
+				System.out.println("\u001B[1;31mPassword cannot be empty\u001B[0m");
+			else {
+				break;
+			}
+		}
+		return loginDB();
+	}
+
+	private void signUpDB() {
+		try {
+			ResultSet rSet = conn.query("SELECT 1 FROM brokers WHERE br_username = '" + this.username + "'");
+
+			if (rSet.next()) {
+				System.out.println("Username already exists!!!");
+				signUp();
+			} else {
+				String pHash = BCrypt.withDefaults().hashToString(10, this.password1.toCharArray());
+				String query = "INSERT INTO brokers ( br_name, br_username, br_password,  br_ip) VALUES (?,?,?,?)";
+				String vals[] = { this.name, this.username, pHash, "1" };
+				int res = conn.preparedStringInsert(query, vals);
+				if (res == 0)
+					throw new SQLException("Something went wrong");
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private boolean loginDB() {
+		try {
+			ResultSet rSet = conn
+					.query("SELECT br_username, br_password FROM brokers WHERE br_username = '" + this.username + "'");
+			if (rSet.next()) {
+				String pHash = rSet.getString("br_password");
+				BCrypt.Result res = BCrypt.verifyer().verify(this.password1.toCharArray(), pHash);
+				if (res.verified) {
+					return true;
+				} else {
+					System.out.println("\nCould not login. Please make sure username and password are correct");
+					this.login();
+				}
+			} else {
+				System.out.println("\nCould not find username. Please make sure username is correct");
+				this.login();
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return false;
+	}
+}
---
+++ market/src/main/java/com/fixme/controlers/MysqlConnect.java
@@ -0,0 +1,95 @@
+package com.fixme.controlers;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ * MysqlConnect
+ */
+
+public final class MysqlConnect {
+	public Connection conn;
+	private Statement statement;
+	private PreparedStatement pStatement;
+	public static MysqlConnect db;
+
+	private MysqlConnect() {
+		String url = "";
+
+		if (System.getenv("USER").equals("rmdaba")) {
+			url = "jdbc:mysql://192.168.99.100:3306/fixme";
+		} else {
+			url = "jdbc:mysql://localhost:3306/fixme";
+		}
+
+		// String dbName = "database_name";
+		String driver = "com.mysql.jdbc.Driver";
+		String userName = "root";
+		String password = "Rootroot3";
+		try {
+			Class.forName(driver).newInstance();
+			this.conn = (Connection) DriverManager.getConnection(url, userName, password);
+			System.out.println("Connection created!");
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.err.println("Could not connect to database : Fixme");
+		}
+	}
+
+	/*
+	 *
+	 * @return MysqlConnect Database connection object
+	 */
+	public static synchronized MysqlConnect getDbCon() {
+		if (db == null) {
+			db = new MysqlConnect();
+		}
+		return db;
+	}
+
+	/*
+	 * @param query String The query to be executed
+	 *
+	 * @return a ResultSet object containing the results or null if not available
+	 *
+	 * @throws SQLException
+	 */
+	public ResultSet query(String query) throws SQLException {
+		statement = db.conn.createStatement();
+		ResultSet res = statement.executeQuery(query);
+		return res;
+	}
+
+	/*
+	 * @desc Method to insert data to a table
+	 *
+	 * @param insertQuery String The Insert query
+	 *
+	 * @return boolean
+	 *
+	 * @throws SQLException
+	 */
+	public int insert(String insertQuery) throws SQLException {
+		statement = db.conn.createStatement();
+		int result = statement.executeUpdate(insertQuery);
+		return result;
+	}
+
+	public int preparedStringInsert(String query, String[] values) throws SQLException {
+		int count = 0;
+		for (int i = 0; i < query.toCharArray().length; i++) {
+			if (query.toCharArray()[i] == '?')
+				count++;
+		}
+		this.pStatement = db.conn.prepareStatement(query);
+		for (int i = 0; i < count; i++) {
+			this.pStatement.setString(i + 1, values[i]);
+		}
+		int result = this.pStatement.executeUpdate();
+		return result;
+	}
+}
